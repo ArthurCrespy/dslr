04326ae2dc967854e00089a132613de54ab45aec
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2025-01-09 14:15:39 +0100

    Add `histogram.py`: dataset parsing and generation of Hogwarts house score analysis

---
+++ files/histogram.py
@@ -0,0 +1,82 @@
+import sys
+import utils
+import matplotlib.pyplot as plt
+
+def statistics_compute(data):
+    stats = {}
+
+    for column, values in data.items():
+        try:
+            values = [float(v) for v in values]
+        except ValueError:
+            continue
+
+        if not values or column == "Index":
+            continue
+
+        stats[column] = {}
+        stats[column]['scores'] = {}
+        stats[column]['stats'] = {}
+
+        for house in set(data['Hogwarts House']):
+            stats[column]['scores'][house] = {}
+            stats[column]['stats'][house] = {}
+            stats[column]['scores'][house] = [v for v, h in zip(values, data['Hogwarts House']) if h == house]
+            stats[column]['stats'][house]['mean'] = utils.maths_mean(stats[column]['scores'][house])
+
+        result = utils.maths_abs((utils.maths_max([stats[column]['stats'][house]['mean'] for house in stats[column]['stats']]) - utils.maths_min([stats[column]['stats'][house]['mean'] for house in stats[column]['stats']])) / utils.maths_min([stats[column]['stats'][house]['mean'] for house in stats[column]['stats']]) * 100)
+        stats[column]['stats']['variation'] = result
+
+    for column, values in stats.items():
+        if stats[column]['stats']['variation'] < result:
+            result = stats[column]['stats']['variation']
+            course = column
+
+    print(f"The course with the most homogeneous score distribution between all four houses is {course} with a variation of {result:.2f}%")
+
+    return stats
+
+
+def statistics_display(stats):
+    plt_combinations = list(stats.items())
+    plt_size = len(plt_combinations)
+
+    fig, axs = plt.subplots(int(utils.maths_sqrt(plt_size) + 1) , int(utils.maths_sqrt(plt_size) + 1), figsize=(25, 25))
+    axs = axs.flatten()
+
+    color = utils.color_house()
+
+    for i, (x, y) in enumerate(plt_combinations):
+        ax = axs[i]
+
+        for j, house in enumerate(stats[x]['scores'].keys()):
+            ax.hist(stats[x]['scores'][house], bins=50, color=color[house], alpha=0.7)
+            ax.set_title(f"Histogram of scores by Hogwarts house for {x}")
+            ax.set_xlabel("Score")
+            ax.set_ylabel("Number of students")
+            ax.text(0.03, 0.95, f"Variation b/w houses : {stats[x]['stats']['variation']:.2f}%", fontsize=10, bbox=dict(facecolor='white', edgecolor='black'), transform=ax.transAxes)
+            ax.grid(axis='x', linestyle='-', alpha=0.3)
+            ax.grid(axis='y', linestyle='-', alpha=0.3)
+
+    for ax in axs[plt_size:]:
+        ax.remove()
+
+    fig.legend(list(color.keys()) , title="Hogwarts Houses", bbox_to_anchor=(0.31, 0.07))
+
+    plt.tight_layout()
+    plt.show()
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: histogram.py <dataset.csv>")
+        sys.exit(1)
+
+    data = utils.csv_parse(sys.argv[1])
+    stats = statistics_compute(data)
+
+    statistics_display(stats)
+
+
+if __name__ == "__main__":
+    main()
